33ccae63b4970d28e0827120fd21846ae56b1ad9
Eduard <nikinna86@gmail.com>
2026-01-07 17:45:56 +0200

    update

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,5 +1,7 @@
 import { Task } from '../models/tasks.js';
 
+import createHttpError from 'http-errors';
+
 export async function getTasksByUserId(req, res) {
   const userId = req.user._id;
 
@@ -34,6 +36,10 @@ export async function updateTaskState(req, res) {
     req.body,
     { new: true },
   );
+
+  if (!task) {
+    throw createHttpError(404, 'Task not found');
+  }
 
   res.status(200).json({
     status: 200,
